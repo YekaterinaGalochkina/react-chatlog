ac809e0ea27ff55f042b85ef03897068b51b5250
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-06-11 15:36:26 -0700

    Adding behavior to heart button

--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,8 +1,17 @@
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
+import { useState } from 'react';
 
 const ChatEntry = (props) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleHeartClick = () => {
+    setIsLiked(!isLiked);
+  };
+
+  const like = isLiked ? '❤️' : '🤍';
+
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{props.sender}</h2>
@@ -11,7 +20,8 @@ const ChatEntry = (props) => {
         <p className="entry-time">
           <TimeStamp time={props.timeStamp} />
         </p>
-        <button className="like">🤍</button>
+        <button className="like"
+          onClick={handleHeartClick}>{like}</button>
       </section>
     </div>
   );
@@ -20,8 +30,7 @@ const ChatEntry = (props) => {
 ChatEntry.propTypes = {
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
-  timeStamp: PropTypes.string.isRequired,
-  liked: PropTypes.bool.isRequired
+  timeStamp: PropTypes.string.isRequired
 };
 
 export default ChatEntry;
